fe37a87fec5e1a6d0b7da0fa111e954d20fa0fa4
Jules Grand <jls.grand@gmail.com>
2021-09-26 08:49:51 +0200

    ajout admin

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
     // On copie le css directement vers la sortie
     eleventyConfig.addPassthroughCopy("style.css");
 
+    eleventyConfig.addPassthroughCopy("admin/config.yml");
+
     // Les langages de template utilisés
     // eleventyConfig.setTemplateFormats(["html", "liquid", "md", "njk"]);
 };
